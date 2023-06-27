df4480676fc71942d45918871c17abf6148baadf
Archana <136048544+Archana-Ragu@users.noreply.github.com>
2023-06-27 07:53:16 -0700

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JavaScript Sample</title>
+</head>
+<body>
+  <script>
+    // JavaScript code goes here
+    // Prompt the user for their name
+    var name = prompt("What is your name?");
+
+    // Display a personalized greeting
+    console.log("Hello, " + name + "! Nice to meet you.");
+  </script>
+</body>
+</html>

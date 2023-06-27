ae5cec0f8335fd59cc3f9cfc1b01071f4e103dfb
Archana <136048544+Archana-Ragu@users.noreply.github.com>
2023-06-27 08:09:40 -0700

    Create bookList.js

---
+++ bookList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BookList from './BookList';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BookList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
